be5a94671c2c338d5fa6ff497e243ca52a6144b2
princeofverry <verrykurniawan@students.undip.ac.id>
2024-02-05 16:56:38 +0700

    nambahin all element dan ganti video

--- src/components/VideoLanding.jsx
+++ src/components/VideoLanding.jsx
@@ -8,6 +8,12 @@ const VideoLanding = () => {
             <div className="Video">
                 <video src={videoBg} autoPlay loop muted />
             </div>
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+                <h1 className='text-8xl text-white font-bold'>
+                    DIENG
+                </h1>
+                <p className='text-2xl text-white font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, unde!</p>
+            </div>
             <div className="absolute bottom-1 h-16 w-16 left-1/2 transform -translate-x-1/2">
                 <img src={scrollGif} className="mx-auto w-full h-auto animate-scrollAnimation" alt="Scrolling GIF" />
             </div>
