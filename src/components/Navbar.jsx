2ae79fb72e83d107f95585020eaa6e66e0d8f60c
princeofverry <verrykurniawan@students.undip.ac.id>
2024-02-06 17:58:31 +0700

    fix navbar with edi festival

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import '../style/Navbar.css'
-import { useState, useEffect } from 'react'
-import logo from '../assets/atasAwan.png'
-import { Link, useNavigate } from 'react-router-dom'
+import React from 'react';
+import '../style/Navbar.css';
+import { useState, useEffect } from 'react';
+import logo from '../assets/atasAwan.png';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
     const navigate = useNavigate();
@@ -16,15 +16,11 @@ const Navbar = () => {
 
     const handleScroll = () => {
         const scrollPosition = window.scrollY;
-        // Set isScrolled to true when the user has scrolled down, and false otherwise
         setIsScrolled(scrollPosition > 0);
     };
 
     useEffect(() => {
-        // Add scroll event listener on mount
         window.addEventListener('scroll', handleScroll);
-
-        // Remove scroll event listener on component unmount
         return () => {
             window.removeEventListener('scroll', handleScroll);
         };
@@ -33,30 +29,30 @@ const Navbar = () => {
     const toggleMenu = () => {
         setIsOpen(!isOpen);
     };
+
     return (
         <>
-            <div className={`navigation bg-${isScrolled ? 'blur bg-white bg-opacity-10 md:shadow-md' : 'transparent'} transition-all duration-300 fixed p-4 top-0 w-full max-h-screen z-50`}>
-                <div className={`container mx-auto flex items-center justify-between ${isScrolled ? 'text-black' : 'text-white'}`}>
+            {/* pengen atasnya putih? */}
+            {/* <div className={`navigation bg-${isScrolled ? 'white' : 'transparent'} md:bg-${isScrolled ? 'transparent' : 'transparent'} ${isScrolled ? 'shadow-md' : ''}  transition-all duration-300 fixed p-4 top-0 w-full z-50`}> */}
+            <div className={`navigation sm:bg-${isScrolled ? 'white bg-white sm:bg-opacity-10 sm:backdrop-filter sm:backdrop-blur-md text-white shadow-md transition-all duration-300' : 'transparent'}  ${isScrolled ? 'shadow-md' : ''} transition-all duration-300 fixed p-4 top-0 w-full z-50`}>
+                <div className="container mx-auto flex items-center justify-between">
                     <div className="flex items-center">
-                        <img src={logo} width={120} alt="logo" className='cursor-pointer' onClick={handleLogoClick} />
+                        <img src={logo} width={120} alt="logo" className="cursor-pointer" onClick={handleLogoClick} />
                     </div>
-                    <div className='hidden md:flex items-center space-x-6 text-lg font-medium'>
-                        <Link to="/Destination" className="link-underline link-underline-black leading-tight hover:text-[#e16a44] transition-all duration-300">Destination</Link>
-                        <Link to="/Accommodation" className="link-underline link-underline-black leading-tight hover:text-[#e16a44] transition-all duration-300">Accommodation</Link>
-                        <Link to="/Machinery" className="link-underline link-underline-black leading-tight hover:text-[#e16a44] transition-all duration-300">Machinery</Link>
-                        <Link to="/Festival" className="link-underline link-underline-black leading-tight hover:text-[#e16a44] transition-all duration-300">Festival</Link>
-                        <Link to="/About-us" className="link-underline link-underline-black leading-tight hover:text-[#e16a44] transition-all duration-300">About Us</Link>
+                    <div className={`hidden md:flex items-center space-x-6 text-lg font-medium text-${isScrolled ? 'black' : 'white'}`}>
+                        <Link to="/Destination" className="link-underline leading-tight hover:text-[#e16a44] transition-all duration-300">Destination</Link>
+                        <Link to="/Accommodation" className="link-underline leading-tight hover:text-[#e16a44] transition-all duration-300">Accommodation</Link>
+                        <Link to="/Machinery" className="link-underline leading-tight hover:text-[#e16a44] transition-all duration-300">Machinery</Link>
+                        <Link to="/Festival" className="link-underline leading-tight hover:text-[#e16a44] transition-all duration-300">Festival</Link>
+                        <Link to="/About-us" className="link-underline leading-tight hover:text-[#e16a44] transition-all duration-300">About Us</Link>
                     </div>
-                    <div className="md:hidden block items-center">
-                        {/* mobile */}
-                        <button onClick={toggleMenu} className={`focus:outline-none ${isOpen || isScrolled ? 'text-black ' : 'text-white'}`}>
+                    <div className="md:hidden block">
+                        <button onClick={toggleMenu} className={`focus:outline-none ${isScrolled || isOpen ? 'text-black' : 'text-white'}`}>
                             {isOpen ? (
-                                // SVG untuk menutup
                                 <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" viewBox="0 0 20 20" fill="currentColor">
                                     <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
                                 </svg>
                             ) : (
-                                // SVG untuk hamburger
                                 <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
                                 </svg>
@@ -65,20 +61,19 @@ const Navbar = () => {
                     </div>
                 </div>
             </div>
-            {
-                isOpen && (
-                    <div className="fixed flex-1 justify-self-center pb-3 md:block md:pb-0 md:mt-0 pt-12 bg-white w-screen text-lg z-200">
-                        <Link to="/Destination" className="block p-4 text-black">Destination</Link>
-                        <Link to="/Accommodation" className="block p-4 text-black">Accommodation</Link>
-                        <Link to="/Machinery" className="block p-4 text-black">Machinery</Link>
-                        <Link to="/Festival" className="block p-4 text-black">Festival</Link>
-                        <Link to="/About-us" className="block p-4 text-black">About Us</Link>
-                    </div >
-                )
-            }
-            {/* Opsi-opsi navigasi untuk tampilan mobile */}
+            {isOpen && (
+                <div className={`md:hidden fixed top-16 left-0 right-0 z-50 bg-${isScrolled ? 'white' : 'transparent'} font-semibold`}>
+                    <div className="container mx-auto py-4 flex flex-col items-center">
+                        <Link to="/Destination" className="block py-2 ">Destination</Link>
+                        <Link to="/Accommodation" className="block py-2 ">Accommodation</Link>
+                        <Link to="/Machinery" className="block py-2 ">Machinery</Link>
+                        <Link to="/Festival" className="block py-2 ">Festival</Link>
+                        <Link to="/About-us" className="block py-2 ">About Us</Link>
+                    </div>
+                </div>
+            )}
         </>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
--- src/page/festival.jsx
+++ src/page/festival.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import festivalCover from '../assets/festivalCover.jpg'
 import lampion from '../assets/lampion.png';
 import ruwatan from '../assets/ruwatan.png';
@@ -8,21 +9,19 @@ import kirabBudaya from '../assets/kirabBudaya.png';
 const Festival = () => {
   return (
     <div className="h-screen w-full">
-      <div className="h-screen">
+      <div className="h-screen w-full">
         <img src={festivalCover} alt="Background" className="h-screen w-full object-cover" />
 
-        {/* Overlay hitam dengan transparansi 50% */}
-        {/* <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div> */}
+        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
 
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
-          <h1 className='md:text-6xl text-6xl text-white font-bold' data-aos="zoom-in" data-aos-anchor-placement="bottom-bottom" data-aos-duration="700">
+          <h1 className='sm:text-6xl text-4xl text-center text-white font-bold' data-aos="zoom-in" data-aos-anchor-placement="bottom-bottom" data-aos-duration="700">
             The Charming Side of Dieng
           </h1>
-          <p className='md:text-5xl text-sm text-white font-semibold' data-aos="zoom-in" data-aos-duration="1000">...</p>
+          <p className='sm:text-6xl text-4xl text-white font-semibold' data-aos="zoom-in" data-aos-duration="1000">...</p>
         </div>
       </div>
 
-      {/* Konten */}
       <div className="p-4 sm:p-8 md:mx-auto bg-[#F9F9F9]">
         <h2 className="text-3xl md:text-4xl font-semibold text-[#E16A44] mb-4">Festival Dieng</h2>
 
@@ -32,28 +31,36 @@ const Festival = () => {
               <span className="font-regular text-gray-800">Filter By:</span>
             </div>
             <div className="flex items-center space-x-4">
-              {/* 4 kotak kecil dengan simbol > */}
               <div className="flex items-center space-x-2">
                 <div className="border border-#271E1B p-2 rounded-md">
                   <span className="text-gray-600">Urutkan</span>
                   <span className="text-[#271E1B] transform rotate-90 ml-1 inline-block">&#8250;</span>
                 </div>
               </div>
-              <div className="flex items-center space-x-2">
-                <div className="border border-#271E1B p-2 rounded-md">
-                  <span className="text-gray-600">Bulan</span>
-                  <span className="text-[#271E1B] transform rotate-90 ml-1 inline-block">&#8250;</span>
+
+              <div className="flex items-center space-x-4">
+                <div className="flex items-center space-x-2">
+                  <div className="border border-#271E1B p-2 rounded-md">
+                    <span className="text-gray-600">Bulan</span>
+                    <span className="text-[#271E1B] transform rotate-90 ml-1 inline-block">&#8250;</span>
+                  </div>
                 </div>
               </div>
-              <div className="flex items-center space-x-2">
-                <div className="border border-#271E1B p-2 rounded-md">
-                  <span className="text-gray-600">Tahun</span>
-                  <span className="text-[#271E1B] transform rotate-90 ml-1 inline-block">&#8250;</span>
+
+              <div className="flex items-center space-x-4">
+                <div className="flex items-center space-x-2">
+                  <div className="border border-#271E1B p-2 rounded-md">
+                    <span className="text-gray-600">Tahun</span>
+                    <span className="text-[#271E1B] transform rotate-90 ml-1 inline-block">&#8250;</span>
+                  </div>
                 </div>
               </div>
-              <div className="flex items-center space-x-2">
-                <div className="border border-#271E1B p-2 rounded-md">
-                  <span className="text-gray-600">Masukkan Teks</span>
+
+              <div className="flex items-center space-x-4">
+                <div className="flex items-center space-x-2">
+                  <div className="border border-#271E1B p-2 rounded-md">
+                    <span className="text-gray-600">Masukkan</span>
+                  </div>
                 </div>
               </div>
             </div>
@@ -61,56 +68,64 @@ const Festival = () => {
         </div>
 
         {/* LAMPION */}
-        <div className="bg-white mb-8 flex flex-col md:flex-row">
-          {/* Bagian kiri gambar */}
-          <div className="md:w-1/3 mb-4 md:mb-0">
-            <img src={lampion} alt="Image" className="w-full h-full object-cover" />
+        <div className="bg-white mb-8 flex flex-col md:flex-row relative">
+          <div className="md:w-1/3 mb-4 md:mb-0 relative">
+            <Link to="/FestivalLampion">
+              <div className="absolute top-0 left-0 w-full h-full" style={{ background: 'linear-gradient(to top, rgba(255,255,255,0.8) 0%, rgba(255,255,255,0) 20%, rgba(0,0,0,0.5) 100%)' }}></div>
+              <img src={lampion} alt="Image" className="w-full h-full object-cover" />
+            </Link>
           </div>
-          {/* Bagian kanan penjelasan */}
-          <div className="md:w-2/3 md:ml-4">
+          <div className="md:w-2/3 md:ml-4 relative">
             <h3 className="text-3xl pt-12 pl-4 font-semibold mb-2">FESTIVAL LAMPION</h3>
             <p className="text-gray-600 pl-4">Penjelasan singkat tentang gambar atau topik tertentu.</p>
+            <div className="absolute bottom-6 right-4 p-4">
+              <Link to="/FestivalLampion" className="text-500 cursor-pointer">See Details &#8250;</Link>
+            </div>
           </div>
         </div>
 
         {/* RUWATAN */}
-        <div className="bg-white mb-8 flex flex-col md:flex-row">
-          {/* Bagian kiri gambar */}
-          <div className="md:w-1/3 mb-4 md:mb-0">
+        <div className="bg-white mb-8 flex flex-col md:flex-row relative">
+          <div className="md:w-1/3 mb-4 md:mb-0 relative">
+            <div className="absolute top-0 left-0 w-full h-full" style={{ background: 'linear-gradient(to top, rgba(255,255,255,0.8) 0%, rgba(255,255,255,0) 20%, rgba(0,0,0,0.5) 100%)' }}></div>
             <img src={ruwatan} alt="Image" className="w-full h-full object-cover" />
           </div>
-          {/* Bagian kanan penjelasan */}
-          <div className="md:w-2/3 md:ml-4">
+          <div className="md:w-2/3 md:ml-4 relative">
             <h3 className="text-3xl pt-12 pl-4 font-semibold mb-2">RUWATAN</h3>
             <p className="text-gray-600 pl-4">Penjelasan singkat tentang gambar atau topik tertentu.</p>
+            <div className="absolute bottom-6 right-4 p-4">
+              <span className="text-500 cursor-pointer">See Details &#8250;</span>
+            </div>
           </div>
         </div>
-
 
         {/* BARITAN */}
-        <div className="bg-white mb-8 flex flex-col md:flex-row">
-          {/* Bagian kiri gambar */}
-          <div className="md:w-1/3 mb-4 md:mb-0">
+        <div className="bg-white mb-8 flex flex-col md:flex-row relative">
+          <div className="md:w-1/3 mb-4 md:mb-0 relative">
+            <div className="absolute top-0 left-0 w-full h-full" style={{ background: 'linear-gradient(to top, rgba(255,255,255,0.8) 0%, rgba(255,255,255,0) 20%, rgba(0,0,0,0.5) 100%)' }}></div>
             <img src={baritan} alt="Image" className="w-full h-full object-cover" />
           </div>
-          {/* Bagian kanan penjelasan */}
-          <div className="md:w-2/3 md:ml-4">
+          <div className="md:w-2/3 md:ml-4 relative">
             <h3 className="text-3xl pt-12 pl-4 font-semibold mb-2">BARITAN</h3>
             <p className="text-gray-600 pl-4">Penjelasan singkat tentang gambar atau topik tertentu.</p>
+            <div className="absolute bottom-6 right-4 p-4">
+              <span className="text-500 cursor-pointer">See Details &#8250;</span>
+            </div>
           </div>
         </div>
 
-
         {/* KIRAB BUDAYA */}
-        <div className="bg-white mb-8 flex flex-col md:flex-row">
-          {/* Bagian kiri gambar */}
-          <div className="md:w-1/3 mb-4 md:mb-0">
+        <div className="bg-white mb-8 flex flex-col md:flex-row relative">
+          <div className="md:w-1/3 mb-4 md:mb-0 relative">
+            <div className="absolute top-0 left-0 w-full h-full" style={{ background: 'linear-gradient(to top, rgba(255,255,255,0.8) 0%, rgba(255,255,255,0) 20%, rgba(0,0,0,0.5) 100%)' }}></div>
             <img src={kirabBudaya} alt="Image" className="w-full h-full object-cover" />
           </div>
-          {/* Bagian kanan penjelasan */}
-          <div className="md:w-2/3 md:ml-4">
+          <div className="md:w-2/3 md:ml-4 relative">
             <h3 className="text-3xl pt-12 pl-4 font-semibold mb-2">KIRAB BUDAYA</h3>
             <p className="text-gray-600 pl-4">Penjelasan singkat tentang gambar atau topik tertentu.</p>
+            <div className="absolute bottom-6 right-4 p-4">
+              <span className="text-500 cursor-pointer">See Details &#8250;</span>
+            </div>
           </div>
         </div>
 
