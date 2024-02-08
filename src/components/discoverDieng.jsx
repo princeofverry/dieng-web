1ca0f1e420fabcc3918911701d93dd84971a89ce
princeofverry <verrykurniawan@students.undip.ac.id>
2024-02-08 19:49:54 +0700

    mempercantik discover dieng

--- src/components/discoverDieng.jsx
+++ src/components/discoverDieng.jsx
@@ -7,7 +7,7 @@ import FestivalDD from '../assets/FestivalDD.png'
 const DiscoverDieng = () => {
     return (
         <>
-            <div className="bg-[#F9F9F9] flex flex-col justify-center items-center py-4 pb-16">
+            <div className="bg-[#F9F9F9] flex flex-col justify-center items-center py-4 pb-16" data-aos="zoom-in-down">
                 <hr className="border-t-4 border-[#e16a44] md:w-24 w-16 mx-auto md:my-8 my-4" />
                 <h1 className="text-[#E16A44] md:text-4xl text-xl font-semibold">
                     Discover Dieng
@@ -22,7 +22,7 @@ const DiscoverDieng = () => {
                         </div>
                         <div className="relative md:px-0 px-4 overflow-hidden">
                             <div className="relative group hover:scale-95 transition-all">
-                                <img src={PenginapanDD} className="w-full transition-transform duration-300 transform-gpu group-hover:rotate-2 filter brightness-50" alt="Destination Image" />
+                                <img src={PenginapanDD} className="w-full transition-transform duration-300 transform-gpu group-hover:-rotate-2 filter brightness-50" alt="Destination Image" />
                                 <h1 className="absolute inset-0 flex items-center justify-center text-white drop-shadow-2xl font-bold text-4xl pointer-events-none">PENGINAPAN</h1>
                             </div>
                         </div>
@@ -34,7 +34,7 @@ const DiscoverDieng = () => {
                         </div>
                         <div className="relative md:px-0 px-4 overflow-hidden">
                             <div className="relative group hover:scale-95 transition-all">
-                                <img src={FestivalDD} className="w-full transition-transform duration-300 transform-gpu group-hover:rotate-2 filter brightness-50" alt="Destination Image" />
+                                <img src={FestivalDD} className="w-full transition-transform duration-300 transform-gpu group-hover:-rotate-2 filter brightness-50" alt="Destination Image" />
                                 <h1 className="absolute inset-0 flex items-center justify-center text-white drop-shadow-2xl font-bold text-4xl pointer-events-none">FESTIVAL</h1>
                             </div>
                         </div>
