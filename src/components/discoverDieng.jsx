261a82877efced41f7bc6aac37cdde66fcc7ef02
princeofverry <verrykurniawan@students.undip.ac.id>
2024-02-08 19:52:25 +0700

    fix aos

--- src/components/discoverDieng.jsx
+++ src/components/discoverDieng.jsx
@@ -7,7 +7,7 @@ import FestivalDD from '../assets/FestivalDD.png'
 const DiscoverDieng = () => {
     return (
         <>
-            <div className="bg-[#F9F9F9] flex flex-col justify-center items-center py-4 pb-16" data-aos="zoom-in-down">
+            <div className="bg-[#F9F9F9] flex flex-col justify-center items-center py-4 pb-16" data-aos="fade-up">
                 <hr className="border-t-4 border-[#e16a44] md:w-24 w-16 mx-auto md:my-8 my-4" />
                 <h1 className="text-[#E16A44] md:text-4xl text-xl font-semibold">
                     Discover Dieng
