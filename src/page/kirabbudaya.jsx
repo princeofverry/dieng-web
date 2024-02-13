ffcc4f9c93c0a417fe064481f01acc4f279f3c14
princeofverry <verrykurniawan@students.undip.ac.id>
2024-02-13 18:47:42 +0700

    perubahan kirab budaya

--- src/page/kirabbudaya.jsx
+++ src/page/kirabbudaya.jsx
@@ -19,27 +19,24 @@ const Kirab = () => {
         <div className="absolute top-0 left-0 w-full h-full bg-black opacity-30"></div>
 
         {/* Kotak Putih */}
-        <div className="absolute top-1/2 transform translate-x-1/2 -translate-y-1/2 text-left max-w-screen-md">
-          <div className="bg-white p-6 rounded-lg relative" style={{ width: '330px', height: '350px' }}>
+        <div className="px-2 absolute top-1/2 -right-0 md:right-1/4 transform md:translate-x-1/2 -translate-y-1/4 md:-translate-y-1/2 text-left">
+          <div className="bg-white md:p-16 p-4 pb-36 rounded-2xl relative">
             <h1 className="text-3xl text-[#262626] font-bold mb-4">KIRAB BUDAYA</h1>
             <div className="flex items-center mb-6">
-              <img src={iconTempat} alt="Icon Tempat" className="mr-3" />
+              <img src={iconTempat} alt="Icon Tempat" className="mr-3 w-1/6" />
               <p className="text-lg">Dieng, Wonosobo</p>
             </div>
             <div className="flex items-center mb-6">
-              <img src={iconTanggal} alt="Icon Tanggal" className="mr-3" />
-              <p className="text-lg">20 Feb 2024</p>
+              <img src={iconTanggal} alt="Icon Tanggal" className="mr-3 w-1/6" />
+              <p className="text-lg underline">20 Feb 2024</p>
             </div>
             <div className="flex items-center mb-6">
-              <img src={iconTiket} alt="Icon Tiket" className="mr-3" />
+              <img src={iconTiket} alt="Icon Tiket" className="mr-3 w-1/6" />
               <p className="text-lg">Gratis</p>
-            </div>
 
-            {/* Kotak Penunjuk Arah */}
-            <div className="absolute">
-              <div className="bg-[#E16A44] text-white p-2 rounded-md">
-                <span className="text-lg">Penunjuk Arah / map</span>
-              </div>
+            </div>
+            <div className="bg-[#E16A44] text-white p-2 rounded-md">
+              <span className="text-lg">Penunjuk Arah / map &#10140; </span>
             </div>
           </div>
         </div>
@@ -49,11 +46,11 @@ const Kirab = () => {
         <h2 className="text-3xl md:text-4xl font-semibold text-[#E16A44] mb-4 px-4 mt-4" data-aos="zoom-in" data-aos-duration="1000">DESKRIPSI</h2>
         <div className="flex items-center mb-2 md:mb-0 px-4" data-aos="zoom-in" data-aos-duration="1000">
           <span className="font-regular text-gray-800">
-            Kirab budaya menjadi salah satu daya tarik Dieng Culture Festival yang banyak menarik wisatawan. 
+            Kirab budaya menjadi salah satu daya tarik Dieng Culture Festival yang banyak menarik wisatawan.
             Pasalnya, acara ini dimulai sebelum dilangsungkan ruwatan atau pemotongan rambut gimbal.
-            Kirab budaya berisi kesenian tradisional yang mengiringi upacara adat ruwatan. 
-            Kesenian tradisional ini mengiringi anak berambut gimbal yang akan dicukur rambutnya. 
-            Festival ini diisi oleh kesenian tradisional jawa, seperti angklung, 
+            Kirab budaya berisi kesenian tradisional yang mengiringi upacara adat ruwatan.
+            Kesenian tradisional ini mengiringi anak berambut gimbal yang akan dicukur rambutnya.
+            Festival ini diisi oleh kesenian tradisional jawa, seperti angklung,
             barongsai kali wetan, lengger, dan masih banyak lagi. Sangat ramai dan meriah!
           </span>
         </div>
