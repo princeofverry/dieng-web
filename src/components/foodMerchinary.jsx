8d44ae913fc731c4eddf97c63c06bee51565fde5
princeofverry <verrykurniawan@students.undip.ac.id>
2024-02-18 10:47:50 +0700

    merapikan foodmerchinary

---
+++ src/components/foodMerchinary.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import carica from '../assets/carica.png';
+import ongklok from '../assets/ongklok.png';
+import jamur from '../assets/jamur.png';
+import tempe from '../assets/tempe.png';
+import kacang from '../assets/kacang.png';
+import buntil from '../assets/buntil.png'
+
+const FoodMerchinary = () => {
+    return (
+        <>
+            <h2 className="text-3xl md:text-4xl font-semibold text-[#E16A44] py-8 text-center">Must-eat Foods</h2>
+            <div className="flex flex-wrap justify-center items-center my-4">
+                <div className="md:w-1/3 md:h-1/3 w-full p-2">
+                    <Link to="/food-details/1">
+                        <div className="flex flex-col items-center">
+                            <img src={carica} alt="Carica" className="w-52 h-52 object-cover rounded-full mb-4" />
+                            <div className='px-8'>
+                                <p className="text-sm text-[#E16A44] font-semibold text-center mb-2">Carica</p>
+                                <p className="text-sm text-gray-500 text-center">Manisan buah carica yang segar, manis dan lembut</p>
+                            </div>
+                        </div>
+                    </Link>
+                </div>
+                <div className="md:w-1/3 md:h-1/3 w-full p-2">
+                    <Link to="/food-details/2">
+                        <div className="flex flex-col items-center">
+                            <img src={ongklok} alt="Mie Ongklok" className="w-52 h-52 object-cover rounded-full mb-4" />
+                            <div className="px-8">
+                                <p className="text-sm text-[#E16A44] font-semibold text-center mb-2">Mie Ongklok</p>
+                                <p className="text-sm text-gray-500 text-center">Mi rebus dengan kuah kental berkanji yang disebut loh dan disajikan bersama sate sapi, kambing, atau ayam</p>
+                            </div>
+                        </div>
+                    </Link>
+                </div>
+                <div className="md:w-1/3 md:h-1/3 w-full p-2">
+                    <Link to="/food-details/3">
+                        <div className="flex flex-col items-center">
+                            <img src={kacang} alt="Kacang dieng" className="w-52 h-52 object-cover rounded-full mb-4" />
+                            <div className="px-8">
+                                <p className="text-sm text-[#E16A44] font-semibold text-center mb-2">Kacang Dieng</p>
+                                <p className="text-sm text-gray-500 text-center">Kacang tanah yang dipanggang dengan bumbu khas</p>
+                            </div>
+                        </div>
+                    </Link>
+                </div>
+                <div className="md:w-1/3 md:h-1/3 w-full p-2">
+                    <Link to="/food-details/4">
+                        <div className="flex flex-col items-center">
+                            <img src={jamur} alt="Keripik Jamur" className="w-52 h-52 object-cover rounded-full mb-4" />
+                            <div className="px-8">
+                                <p className="text-sm text-[#E16A44] font-semibold text-center mb-2">Keripik Jamur</p>
+                                <p className="text-sm text-gray-500 text-center">Keripik dari jamur kancing yang digoreng dan diberi bumbu khas</p>
+                            </div>
+                        </div>
+                    </Link>
+                </div>
+                <div className="md:w-1/3 md:h-1/3 w-full p-2">
+                    <Link to="/food-details/5">
+                        <div className="flex flex-col items-center">
+                            <img src={tempe} alt="Tempe Kemul" className="w-52 h-52 object-cover rounded-full mb-4" />
+                            <div className="px-8">
+                                <p className="text-sm text-[#E16A44] font-semibold mb-2 text-center">Tempe Kemul</p>
+                                <p className="text-sm text-gray-500 text-center">
+                                    Tempe goreng dibalut gandum dan campuran tepung singkong serta beras, biasanya dengan daun kucai.</p>
+                            </div>
+                        </div>
+                    </Link>
+                </div>
+                <div className="md:w-1/3 md:h-1/3 w-full p-2">
+                    <Link to="/food-details/6">
+                        <div className="flex flex-col items-center">
+                            <img src={buntil} alt="Buntil Singkong" className="w-52 h-52 object-cover rounded-full mb-4" />
+                            <div className="px-8">
+                                <p className="text-sm text-[#E16A44] font-semibold mb-2 text-center">Buntil Singkong</p>
+                                <p className="text-sm text-gray-500 text-center">Bungkusan talas atau daun singkong yang berisi parutan kelapa dan bumbu pedas yang terbuat dari santan.</p>
+                            </div>
+                        </div>
+                    </Link>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default FoodMerchinary
--- src/page/merchinary.jsx
+++ src/page/merchinary.jsx
@@ -1,15 +1,7 @@
-
-import AOS from 'aos';
 import 'aos/dist/aos.css';
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import merchinaryCover from '../assets/merchinaryCover.png';
-import carica from '../assets/carica.png';
-import ongklok from '../assets/ongklok.png';
-import jamur from '../assets/jamur.png';
-import tempe from '../assets/tempe.png';
-import kacang from '../assets/kacang.png';
-import buntil from '../assets/buntil.png'
 import edelweis from '../assets/edelweis.png'
 import kaos from '../assets/kaos.png'
 import syal from '../assets/syal.png'
@@ -19,6 +11,7 @@ import belerang from '../assets/belerang.png'
 import miniatur from '../assets/miniatur.png'
 import Boemi from '../assets/boemi.png'
 import maknohan from '../assets/maknohan.png'
+import FoodMerchinary from '../components/foodMerchinary';
 
 const items = [
   {
@@ -55,7 +48,7 @@ const items = [
 ];
 
 const Merchinary = () => {
-  
+
   return (
     <div className="">
       {/* Hero section */}
@@ -72,73 +65,10 @@ const Merchinary = () => {
       </div>
 
       {/* Foods section */}
-      <div className="mx-auto bg-white flex flex-col justify-center items-center py-4">
-        <h2 className="text-3xl md:text-4xl font-semibold text-[#E16A44] mb-4 px-4 mt-4">Must-eat Foods</h2>
-
-        <div className="flex flex-wrap justify-center items-center mt-8">
-          <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
-            <Link to="/food-details/1">
-              <div className="flex flex-col items-center">
-                <img src={carica} alt="Carica" className="w-32 h-32 object-cover rounded-full mb-2" />
-                <p className="text-sm text-[#E16A44] font-semibold">Carica</p>
-                <p className="text-sm text-gray-500 text-center">Manisan buah carica yang segar, manis dan lembut</p>
-              </div>
-            </Link>
-          </div>
-          <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
-            <Link to="/food-details/2">
-              <div className="flex flex-col items-center">
-                <img src={ongklok} alt="Mie Ongklok" className="w-32 h-32 object-cover rounded-full mb-2" />
-                <p className="text-sm text-[#E16A44] font-semibold">Mie Ongklok</p>
-                <p className="text-sm text-gray-500 text-center">Mi rebus dengan kuah kental berkanji yang disebut loh dan disajikan bersama sate sapi, kambing, atau ayam</p>
-              </div>
-            </Link>
-          </div>
-          <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
-            <Link to="/food-details/3">
-              <div className="flex flex-col items-center">
-                <img src={kacang} alt="Kacang dieng" className="w-32 h-32 object-cover rounded-full mb-2" />
-                <p className="text-sm text-[#E16A44] font-semibold">Kacang Dieng</p>
-                <p className="text-sm text-gray-500 text-center">Kacang tanah yang dipanggang dengan bumbu khas</p>
-              </div>
-            </Link>
-          </div>
-          <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
-            <Link to="/food-details/4">
-              <div className="flex flex-col items-center">
-                <img src={jamur} alt="Keripik Jamur" className="w-32 h-32 object-cover rounded-full mb-2" />
-                <p className="text-sm text-[#E16A44] font-semibold">Keripik Jamur</p>
-                <p className="text-sm text-gray-500 text-center">Keripik dari jamur kancing yang digoreng dan diberi bumbu khas</p>
-              </div>
-            </Link>
-          </div>
-          <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
-            <Link to="/food-details/5">
-              <div className="flex flex-col items-center">
-                <img src={tempe} alt="Tempe Kemul" className="w-32 h-32 object-cover rounded-full mb-2" />
-                <p className="text-sm text-[#E16A44] font-semibold">Tempe Kemul</p>
-                <p className="text-sm text-gray-500 text-center">Tempe yang digoreng dengan dibalut gandum, pati/ tepung singkong juga tepung beras dan biasanya ditambah daun kucai untuk pelengkap.</p>
-              </div>
-            </Link>
-          </div>
-          <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
-            <Link to="/food-details/6">
-              <div className="flex flex-col items-center">
-                <img src={buntil} alt="Buntil Singkong" className="w-32 h-32 object-cover rounded-full mb-2" />
-                <p className="text-sm text-[#E16A44] font-semibold">Buntil Singkong</p>
-                <p className="text-sm text-gray-500 text-center">Bungkusan talas atau daun singkong yang berisi parutan kelapa dan bumbu pedas yang terbuat dari santan.</p>
-              </div>
-            </Link>
-          </div>
-          
-          
-        </div>
-      </div>
-      
+      <FoodMerchinary />
 
       {/* suvenir */}
       <div className="flex justify-center items-center bg-[#F9F9F9]">
-      
         <div className="w-full md:w-1/2 lg:w-1/3 p-4">
           <div className="w-full text-center py-4">
             <h2 className="text-3xl font-bold text-[#E16A44] mb-4">Must-have Souvenirs</h2>
@@ -167,14 +97,14 @@ const Merchinary = () => {
               </Link>
             </div>
           ))}
+        </div>
       </div>
-    </div>
 
-    {/* dimanaa*/}
-    {/* gambar dikiri */}
-    <div className="mx-auto bg-white flex flex-col justify-center items-center py-4">
-      <h2 className="text-3xl md:text-4xl font-semibold text-[#E16A44] mb-4 px-4 mt-4">Where to find it?</h2>
-       <div className="bg-[#F9F9F9] mx-8 mt-12 flex flex-col md:flex-row rounded overflow-hidden">
+      {/* dimanaa*/}
+      {/* gambar dikiri */}
+      <div className="mx-auto bg-white flex flex-col justify-center items-center py-4">
+        <h2 className="text-3xl md:text-4xl font-semibold text-[#E16A44] mb-4 px-4 mt-4">Where to find it?</h2>
+        <div className="bg-[#F9F9F9] mx-8 mt-12 flex flex-col md:flex-row rounded overflow-hidden">
           {/* Image Section */}
           <div className="md:w-1/3 relative">
             <Link to="/Boemi">
@@ -193,23 +123,23 @@ const Merchinary = () => {
       </div>
 
 
-    {/* gambar dikanan */}
-    <div className="bg-[#F9F9F9] mx-8 my-12 flex flex-col md:flex-row rounded overflow-hidden">
-      {/* Text Content Section */}
-      <div className="md:w-2/3 md:mr-4 md:pr-4 relative">
-        <h3 className="text-xl md:text-3xl text-[#E16A44] pt-6 font-semibold mb-2">MakNohan</h3>
-        <p className="text-gray-600">Pusat oleh-oleh makanan khas Dieng Wonosobo yang menyediakan berbagai jenis oleh-oleh, mulai dari makanan, cemilan, baju, souvenir hingga kain khas dengan harga terjangkau dan kualitas terbaik.</p>
-        <button className="bg-[#E16A44] text-white py-2 px-4 mt-4">Maps</button>
-      </div>
+      {/* gambar dikanan */}
+      <div className="bg-[#F9F9F9] mx-8 my-12 flex flex-col md:flex-row rounded overflow-hidden">
+        {/* Text Content Section */}
+        <div className="md:w-2/3 md:mr-4 md:pr-4 relative">
+          <h3 className="text-xl md:text-3xl text-[#E16A44] pt-6 font-semibold mb-2">MakNohan</h3>
+          <p className="text-gray-600">Pusat oleh-oleh makanan khas Dieng Wonosobo yang menyediakan berbagai jenis oleh-oleh, mulai dari makanan, cemilan, baju, souvenir hingga kain khas dengan harga terjangkau dan kualitas terbaik.</p>
+          <button className="bg-[#E16A44] text-white py-2 px-4 mt-4">Maps</button>
+        </div>
 
-      {/* Image Section */}
-      <div className="md:w-1/3 relative">
-        <Link to="/maknohan">
-          <div className="absolute top-0 left-0 w-full h-60"></div>
-          <img src={maknohan} alt="MakNohan" className="w-full h-60 object-cover" />
-        </Link>
+        {/* Image Section */}
+        <div className="md:w-1/3 relative">
+          <Link to="/maknohan">
+            <div className="absolute top-0 left-0 w-full h-60"></div>
+            <img src={maknohan} alt="MakNohan" className="w-full h-60 object-cover" />
+          </Link>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
