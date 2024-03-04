b4d7312de91f216583aa281f6e81b2b5ac8aecef
princeofverry <verrykurniawan@students.undip.ac.id>
2024-03-04 19:09:15 +0700

    animation merchinary sama ubah logo

--- src/page/merchinary.jsx
+++ src/page/merchinary.jsx
@@ -16,20 +16,26 @@ const Merchinary = () => {
         <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
 
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
-          <h1 className='sm:text-8xl text-4xl text-center text-white font-bold' data-aos="zoom-in" data-aos-anchor-placement="bottom-bottom" data-aos-duration="700">
+          <h1 className='sm:text-6xl lg:text-8xl text-4xl text-center text-white font-bold' data-aos="zoom-in" data-aos-anchor-placement="bottom-bottom" data-aos-duration="700">
             Taste of Dieng
           </h1>
         </div>
       </div>
 
       {/* Foods section ada di components */}
-      <FoodMerchinary />
+      <div data-aos="fade-up" data-aos-duration="500">
+        <FoodMerchinary />
+      </div>
 
       {/* suvenir ada di components*/}
-      <HaveSouvenirs />
+      <div data-aos="fade-up" data-aos-duration="500">
+        <HaveSouvenirs />
+      </div>
 
       {/* gambar dikiri dan kanan*/}
-      <WhereToFindIt />
+      <div data-aos="fade-up" data-aos-duration="500">
+        <WhereToFindIt />
+      </div>
     </div>
   );
 }
